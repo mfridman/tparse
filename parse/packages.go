7ecf89b4f016588d3f8215a13ea7c7dfacbd2b49
Michael Fridman <mf192@icloud.com>
2018-10-27 01:12:03 -0400

    Fixes #11; adds colors to coverage column

--- parse/packages.go
+++ parse/packages.go
@@ -13,47 +13,58 @@ type Packages map[string]*Package
 
 func (p Packages) PrintSummary(skipNoTests bool) {
 	tbl := tablewriter.NewWriter(os.Stdout)
-
 	tbl.SetHeader([]string{
-		"Status",
-		"Elapsed",
-		"Package",
-		"Cover",
-		"Pass",
-		"Fail",
-		"Skip",
+		"Status",  //0
+		"Elapsed", //1
+		"Package", //2
+		"Cover",   //3
+		"Pass",    //4
+		"Fail",    //5
+		"Skip",    //6
 	})
 
 	for name, pkg := range p {
 
 		if pkg.NoTest {
-			if !skipNoTests {
-				continue
+			if skipNoTests {
+				tbl.Append([]string{
+					colorize("SKIP", cYellow, true),
+					"--",
+					name + "\n[no test files]",
+					fmt.Sprintf(" %.1f%%", pkg.Coverage),
+					"--", "--", "--",
+				})
 			}
-
-			tbl.Append([]string{
-				colorize("SKIP", cYellow, true),
-				"0.00s",
-				name + "\n[no test files]",
-				fmt.Sprintf(" %.1f%%", pkg.Coverage),
-				"0", "0", "0",
-			})
-
 			continue
 		}
 
+		var elapsed string
 		if pkg.Cached {
-			name += " (cached)"
+			elapsed = "(cached)"
+		} else {
+			elapsed = strconv.FormatFloat(pkg.Summary.Elapsed, 'f', 2, 64) + "s"
+		}
+
+		coverage := fmt.Sprintf("%.1f%%", pkg.Coverage)
+		switch c := pkg.Coverage; {
+		case c == 0.0:
+			break
+		case c <= 50.0:
+			coverage = colorize(coverage, cRed, true)
+		case pkg.Coverage > 50.0 && pkg.Coverage < 80.0:
+			coverage = colorize(coverage, cYellow, true)
+		case pkg.Coverage >= 80.0:
+			coverage = colorize(coverage, cGreen, true)
 		}
 
 		tbl.Append([]string{
-			pkg.Summary.Action.WithColor(),
-			strconv.FormatFloat(pkg.Summary.Elapsed, 'f', 2, 64) + "s",
-			name,
-			fmt.Sprintf(" %.1f%%", pkg.Coverage),
-			strconv.Itoa(len(pkg.TestsByAction(ActionPass))),
-			strconv.Itoa(len(pkg.TestsByAction(ActionFail))),
-			strconv.Itoa(len(pkg.TestsByAction(ActionSkip))),
+			pkg.Summary.Action.WithColor(), //0
+			elapsed,                        //1
+			name,                           //2
+			coverage,                       //3
+			strconv.Itoa(len(pkg.TestsByAction(ActionPass))), //4
+			strconv.Itoa(len(pkg.TestsByAction(ActionFail))), //5
+			strconv.Itoa(len(pkg.TestsByAction(ActionSkip))), //6
 		})
 	}
 
