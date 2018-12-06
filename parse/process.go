5f8f0d9f66cc9bd08f1f81a5d69224f15c52b4e1
Michael Fridman <mf192@icloud.com>
2018-12-05 19:28:46 -0500

    Update parse/process.go

--- parse/process.go
+++ parse/process.go
@@ -128,6 +128,7 @@ func ReplayOutput(w io.Writer, r io.Reader) {
 			fmt.Fprintln(w, strings.TrimSpace(sc.Text()))
 			continue
 		}
+		// Output expected to be terminated by a newline.
 		fmt.Fprint(w, e.Output)
 	}
 
