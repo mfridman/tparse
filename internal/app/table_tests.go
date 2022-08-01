19cbd36ee89a8358acd387bc2067b4fa9a40981d
Michael Fridman <mf192@icloud.com>
2022-07-31 21:09:44 -0400

    Add markdown section for tests table (#80)

--- internal/app/table_tests.go
+++ internal/app/table_tests.go
@@ -31,6 +31,13 @@ type TestTableOptions struct {
 	Slow int
 }
 
+type packageTests struct {
+	skippedCount int
+	skipped      []*parse.Test
+	passedCount  int
+	passed       []*parse.Test
+}
+
 func (c *consoleWriter) testsTable(packages []*parse.Package, option TestTableOptions) {
 	// Print passed tests, sorted by elapsed DESC. Grouped by alphabetically sorted packages.
 	var tableString strings.Builder
@@ -49,45 +56,17 @@ func (c *consoleWriter) testsTable(packages []*parse.Package, option TestTableOp
 		if pkg.NoTestFiles || pkg.NoTests || pkg.HasPanic {
 			continue
 		}
-		var skipped, passed []*parse.Test
-
-		if option.Skip {
-			skipped = append(skipped, pkg.TestsByAction(parse.ActionSkip)...)
-		}
-		if option.Pass {
-			passed = append(passed, pkg.TestsByAction(parse.ActionPass)...)
-
-			// Order passed tests within a package by elapsed time DESC (longest on top).
-			sort.Slice(passed, func(i, j int) bool {
-				return passed[i].Elapsed() > passed[j].Elapsed()
-			})
-			// Optionall, display only the slowest N tests by elapsed time.
-			if option.Slow > 0 && len(passed) > option.Slow {
-				passed = passed[:option.Slow]
-			}
-		}
-
-		all := make([]*parse.Test, 0, len(skipped)+len(passed))
-		all = append(all, skipped...)
-		all = append(all, passed...)
+		pkgTests := getTestsFromPackages(pkg, option)
+		all := make([]*parse.Test, 0, len(pkgTests.passed)+len(pkgTests.skipped))
+		all = append(all, pkgTests.passed...)
+		all = append(all, pkgTests.skipped...)
 
 		for _, t := range all {
-			// TODO(mf): why are we sorting this? Remove it and see if it breaks anything.
+			// TODO(mf): why are we sorting this?
 			t.SortEvents()
 
-			var testName strings.Builder
-			testName.WriteString(t.Name)
-			if option.Trim && testName.Len() > 32 && strings.Count(testName.String(), "/") > 0 {
-				testName.Reset()
-				ss := strings.Split(t.Name, "/")
-				testName.WriteString(ss[0] + "\n")
-				for i, s := range ss[1:] {
-					testName.WriteString(" /" + s)
-					if i != len(ss[1:])-1 {
-						testName.WriteString("\n")
-					}
-				}
-			}
+			testName := shortenTestName(t.Name, option.Trim)
+
 			status := strings.ToUpper(t.Status().String())
 			switch t.Status() {
 			case parse.ActionPass:
@@ -108,7 +87,7 @@ func (c *consoleWriter) testsTable(packages []*parse.Package, option TestTableOp
 			row := testRow{
 				status:      status,
 				elapsed:     strconv.FormatFloat(t.Elapsed(), 'f', 2, 64),
-				testName:    testName.String(),
+				testName:    testName,
 				packageName: packageName,
 			}
 			tbl.Append(row.toRow())
@@ -131,6 +110,114 @@ func (c *consoleWriter) testsTable(packages []*parse.Package, option TestTableOp
 		}
 		fmt.Fprintln(c.w, output)
 	}
+}
+
+func (c *consoleWriter) testsTableMarkdown(packages []*parse.Package, option TestTableOptions) {
+	for _, pkg := range packages {
+		// Print passed tests, sorted by elapsed DESC. Grouped by alphabetically sorted packages.
+		var tableString strings.Builder
+		tbl := newTableWriter(&tableString, c.format)
+
+		header := []string{
+			"Status",
+			"Elapsed",
+			"Test",
+		}
+		tbl.SetHeader(header)
+
+		// Discard packages where we cannot generate a sensible test summary.
+		if pkg.NoTestFiles || pkg.NoTests || pkg.HasPanic {
+			continue
+		}
+		pkgTests := getTestsFromPackages(pkg, option)
+		all := make([]*parse.Test, 0, len(pkgTests.passed)+len(pkgTests.skipped))
+		all = append(all, pkgTests.passed...)
+		all = append(all, pkgTests.skipped...)
+
+		for _, t := range all {
+			// TODO(mf): why are we sorting this?
+			t.SortEvents()
+
+			testName := shortenTestName(t.Name, option.Trim)
+
+			status := strings.ToUpper(t.Status().String())
+			switch t.Status() {
+			case parse.ActionPass:
+				status = c.green(status)
+			case parse.ActionSkip:
+				status = c.yellow(status)
+			case parse.ActionFail:
+				status = c.red(status)
+			}
+			tbl.Append([]string{
+				status,
+				strconv.FormatFloat(t.Elapsed(), 'f', 2, 64),
+				testName,
+			})
+		}
+		if tbl.NumLines() > 0 {
+			tbl.Render()
+
+			fmt.Fprintf(c.w, "## 📦 Package **`%s`**\n", pkg.Summary.Package)
+			fmt.Fprintln(c.w)
+			fmt.Fprintf(c.w,
+				"**%d passed** tests (out of %d) | **%d skipped** tests (out of %d)\n",
+				len(pkgTests.passed),
+				pkgTests.passedCount,
+				len(pkgTests.skipped),
+				pkgTests.skippedCount,
+			)
+			fmt.Fprintln(c.w)
+			fmt.Fprintln(c.w, "<details>")
+			fmt.Fprintln(c.w)
+			fmt.Fprintln(c.w, "<summary>Click for test summary</summary>")
+			fmt.Fprintln(c.w)
+			fmt.Fprintln(c.w, tableString.String())
+			fmt.Fprintln(c.w, "</details>")
+			fmt.Fprintln(c.w)
+		}
+		fmt.Fprintln(c.w)
+	}
+}
+
+func getTestsFromPackages(pkg *parse.Package, option TestTableOptions) *packageTests {
+	tests := &packageTests{}
+	skipped := pkg.TestsByAction(parse.ActionSkip)
+	tests.skippedCount = len(skipped)
+	passed := pkg.TestsByAction(parse.ActionPass)
+	tests.passedCount = len(passed)
+	if option.Skip {
+		tests.skipped = append(tests.skipped, skipped...)
+	}
+	if option.Pass {
+		tests.passed = append(tests.passed, passed...)
+		// Order passed tests within a package by elapsed time DESC (longest on top).
+		sort.Slice(tests.passed, func(i, j int) bool {
+			return tests.passed[i].Elapsed() > tests.passed[j].Elapsed()
+		})
+		// Optionall, display only the slowest N tests by elapsed time.
+		if option.Slow > 0 && len(tests.passed) > option.Slow {
+			tests.passed = tests.passed[:option.Slow]
+		}
+	}
+	return tests
+}
+
+func shortenTestName(s string, trim bool) string {
+	var testName strings.Builder
+	testName.WriteString(s)
+	if trim && testName.Len() > 32 && strings.Count(testName.String(), "/") > 0 {
+		testName.Reset()
+		ss := strings.Split(s, "/")
+		testName.WriteString(ss[0] + "\n")
+		for i, s := range ss[1:] {
+			testName.WriteString(" /" + s)
+			if i != len(ss[1:])-1 {
+				testName.WriteString("\n")
+			}
+		}
+	}
+	return testName.String()
 }
 
 type testRow struct {
