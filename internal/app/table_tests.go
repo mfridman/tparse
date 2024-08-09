2d3f361dd574b39221386a42cff36e1736e29613
Amir Maleki <32163972+thisisamirmaleki@users.noreply.github.com>
2024-08-09 15:47:39 +0330

    Add test name text wrapping on small screens (#124)
    Co-authored-by: Amir Maleki <amirmal@systemgroup.net>

--- internal/app/table_tests.go
+++ internal/app/table_tests.go
@@ -208,7 +208,12 @@ func shortenTestName(s string, trim bool, maxLength int) string {
 		ss := strings.Split(s, "/")
 		testName.WriteString(ss[0] + "\n")
 		for i, s := range ss[1:] {
-			testName.WriteString(" /" + s)
+			testName.WriteString(" /")
+			for len(s) > maxLength {
+				testName.WriteString(s[:maxLength-2] + " …\n  ")
+				s = s[maxLength-2:]
+			}
+			testName.WriteString(s)
 			if i != len(ss[1:])-1 {
 				testName.WriteString("\n")
 			}
